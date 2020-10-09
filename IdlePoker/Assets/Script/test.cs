7fc06b3f3c5d9bb9d07035cbcbca477d4286f155
Appopiss <appopiss1840@gmail.com>
2020-10-09 19:26:48 +0900

    test is added

---
+++ IdlePoker/Assets/Script/test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using static UsefulMethod;
+using static BASE;
+using TMPro;
+
+public class test : BASE {
+
+	// Use this for initialization
+	void Awake () {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
